43320c856b5e24b3258f11de876e267bad1ddc0a
Vignesh5605 <vigneshramalingam305@gmail.com>
2025-09-29 18:34:27 +0530

    jwt implemented

--- eventify-backend/app.js
+++ eventify-backend/app.js
@@ -7,6 +7,10 @@ const { getEvents } = require("./controllers/eventController");
 const { updateEvent } = require("./controllers/eventController");
 const { deleteEvent } = require("./controllers/eventController");
 
+const { registerUser, loginUser } = require("./controllers/userController");
+
+
+
 const app = express();
 app.use(express.json());
 
@@ -14,6 +18,11 @@ app.post("/api/events", createEvent);
 app.get("/api/events", getEvents);
 app.put("/api/events/:id", updateEvent);
 app.delete("/api/events/:id", deleteEvent);
+
+app.post("/api/users/register", registerUser);
+app.post("/api/users/login", loginUser);
+
+
 
 mongoose
   .connect(process.env.MONGO_URI)
